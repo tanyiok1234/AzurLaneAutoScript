4f70eff362106dcfe7a06370c68d926689f840bc
QwQ <tanyiok1234@users.noreply.github.com>
2024-04-07 02:31:10 +0900

    Test3

--- module/config/config.py
+++ module/config/config.py
@@ -292,14 +292,17 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
 
         force_enable(
             [
+                "Commission", 
+                "Research", 
+                "Reward", 
             ]
         )
-        limit_next_run(["Commission", "Reward"], limit=now + timedelta(hours=12, seconds=-1))
-        limit_next_run(["Research"], limit=now + timedelta(hours=24, seconds=-1))
+        limit_next_run(["Commission", "Reward"], limit=now + timedelta(hours=9999, seconds=-1))
+        limit_next_run(["Research"], limit=now + timedelta(hours=9999, seconds=-1))
         limit_next_run(["OpsiExplore", "OpsiCrossMonth", "OpsiVoucher", "OpsiMonthBoss"],
                        limit=now + timedelta(days=31, seconds=-1))
-        limit_next_run(["OpsiArchive"], limit=now + timedelta(days=7, seconds=-1))
-        limit_next_run(self.args.keys(), limit=now + timedelta(hours=24, seconds=-1))
+        limit_next_run(["OpsiArchive"], limit=now + timedelta(days=9999, seconds=-1))
+        limit_next_run(self.args.keys(), limit=now + timedelta(hours=9999, seconds=-1))
 
         """
         Override anything you want.
